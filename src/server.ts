a410fd977f51182f05bc2b353d2fec95b5a9e674
henriquegheno <euhenriquegheno@gmail.com>
2026-07-07 21:01:59 -0300

    Configurações do typescript

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import { env } from './env'
-import { app } from './app';
+import { app } from './app'
 
-app.listen({
-  host: '0.0.0.0',
-  port: env.PORT
-}).then(() => {
-  console.log('Server is running on https://localhost:' + env.PORT);
-})
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('Server is running on https://localhost:' + env.PORT)
+  })
